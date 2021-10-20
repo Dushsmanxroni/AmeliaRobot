831007f62596afb32c2895a14e32f5b0f069c0d9
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-10-20 10:59:46 +0530

    Update misc.py

--- AmeliaRobot/modules/helper_funcs/misc.py
+++ AmeliaRobot/modules/helper_funcs/misc.py
@@ -21,7 +21,7 @@ def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
 
-    lines = msg.splitlines(True)
+    lines = msg.splitlines(False)
     small_msg = ""
     result = []
     for line in lines:
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔺", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            (EqInlineKeyboardButton("«", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Back", callback_data="asuna_back"),
-             EqInlineKeyboardButton("🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("»", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="asuna_back")]]
+        pairs += [[EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="asuna_back")]]
 
     return pairs
 
